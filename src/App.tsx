d97ca7a3c505bf1ce1a03c6b78f1b32c373d11d4
Chloe Ashley <chloeashley193@gmail.com>
2025-01-30 14:31:35 +0000

    adding tailwindcss v4

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Header } from './components/Header.tsx'
 function App() {
     return (
         <>
-            <Header title="foragery" description="Your Trusted Companion for Edible Plants." />
+            <Header title="foragery" description="Your Trusted Companion for Edible Plants." className="font-sans text-blue-900" />
         </>
     )
 }
