552086bc9585175bfa246c215def4fb1fffdfba1
Chloe Ashley <chloeashley193@gmail.com>
2025-02-06 14:37:49 +0000

    a11y test setup

--- test/components/ui/SearchBar.test.tsx
+++ test/components/ui/SearchBar.test.tsx
@@ -1,24 +1,36 @@
-import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
+import { axe } from 'vitest-axe'
+import { describe, it, expect, vi } from 'vitest'
 
 import { SearchBar } from '../../../src/components/ui/SearchBar'
-import { describe } from 'vitest'
-
 
 describe('SearchBar', () => {
     const handleChange = vi.fn()
 
     it('renders the SearchBar', () => {
-        render(<SearchBar onChange={handleChange} />)
-        expect(screen.getByRole('searchbox', { name: 'Search for a plant' })).toBeInTheDocument()
+        render(<SearchBar onChange={handleChange} inputClassName={"testInputClassName"} />)
+        const element: HTMLElement = screen.getByRole('searchbox', {
+            name: 'Search for a plant',
+        })
+        expect(element).toBeInTheDocument()
+        expect(element.className).toEqual("testInputClassName p-1 border-2 border-black focus:outline-none");
     })
 
     it('calls onChange handler when user types', async () => {
         render(<SearchBar onChange={handleChange} />)
-        const input: string = "Thyme"
+        const input: string = 'Thyme'
 
-        await userEvent.type(screen.getByRole('searchbox', { name: 'Search for a plant' }), input)
+        await userEvent.type(
+            screen.getByRole('searchbox', { name: 'Search for a plant' }),
+            input
+        )
         expect(handleChange).toHaveBeenCalledTimes(input.length)
+    })
+
+    it('SearchBar component should have no accessibility violations', async () => {
+        const { container } = render(<SearchBar onChange={handleChange} />)
+        const results = await axe(container)
+        expect(results.violations).toEqual([])
     })
 })
---
+++ vitest.setup.ts
@@ -0,0 +1,6 @@
+import { expect } from 'vitest'
+import { vi } from "vitest";
+import * as matchers from '@testing-library/jest-dom/matchers'
+
+expect.extend(matchers)
+HTMLCanvasElement.prototype.getContext = vi.fn();
